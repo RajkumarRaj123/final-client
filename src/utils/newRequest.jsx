35dd63406047afebb65df709459e1b5c1c61b4fb
RajkumarRaj123 <rajkumarviratraj07583@gmail.com>
2025-11-18 17:56:27 +0530

    first

--- src/utils/newRequest.jsx
+++ src/utils/newRequest.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://final-server1.onrender.com/api",
+
   withCredentials: true,
 });
 
