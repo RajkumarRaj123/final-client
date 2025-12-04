f8d8704f8b246649d5d80161adddbef96b505db2
RajkumarRaj123 <rajkumarviratraj07583@gmail.com>
2025-12-04 16:09:33 +0530

    first commit

--- src/utils/uploads.jsx
+++ src/utils/uploads.jsx
@@ -1,14 +1,15 @@
-import axios from "axios";
+import newRequest from "./newRequest";
 
 const upload = async (file) => {
   const data = new FormData();
   data.append("file", file);
   data.append("upload_preset", "fiverr");
   try {
-    const res = await axios.post(
+    const res = await newRequest.post(
       "http://api.cloudinary.com/v1_1/dfb3cxoyi/image/upload",
       data
     );
+    console.log(res.data);
     const { url } = res.data;
     return url;
   } catch (err) {
