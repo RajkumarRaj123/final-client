219db8309c4052eadfdd2d5828038c07f0aeb079
RajkumarRaj123 <rajkumarviratraj07583@gmail.com>
2025-12-19 17:15:38 +0530

    first commit

--- src/utils/newRequest.jsx
+++ src/utils/newRequest.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://final-server2-1.onrender.com/api",
 
   withCredentials: true,
   headers: {
