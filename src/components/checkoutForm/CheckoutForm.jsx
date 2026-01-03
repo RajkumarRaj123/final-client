29193e08f8c73d90485b9a07216b5e87ca622cbc
RajkumarRaj123 <rajkumarviratraj07583@gmail.com>
2026-01-03 16:13:15 +0530

    Fix component filename case

--- src/components/checkoutForm/CheckoutForm.jsx
+++ src/components/checkoutForm/CheckoutForm.jsx
@@ -57,7 +57,7 @@ const CheckoutForm = () => {
     const { error } = await stripe.confirmPayment({
       elements,
       confirmParams: {
-        return_url: "https://bright-praline-7d1852.netlify.app/success",
+        return_url: "https://stellular-donut-391f25.netlify.app/success",
       },
     });
 
--- src/utils/newRequest.jsx
+++ src/utils/newRequest.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://final-server2-4zoh.onrender.com/api",
   withCredentials: true,
 });
 
